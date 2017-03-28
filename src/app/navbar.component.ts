cfe1605f8f595a52b4f95791d8100850cb93a699
Brian May <brian@linuxpenguins.xyz>
2017-03-28 19:53:57 +1100

    Simplify collapsing of menu

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -5,12 +5,5 @@ import { Component } from '@angular/core';
     templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-    toggler_class : string = "collapse";
-    toggler() {
-        if (this.toggler_class === "") {
-            this.toggler_class = "collapse";
-        } else {
-            this.toggler_class = "";
-        }
-    }
+    private isCollapsed : boolean = true;
 }
