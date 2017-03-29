d38059afcf51dbe352481e61335829eaa57cf480
Brian May <brian@linuxpenguins.xyz>
2017-03-29 17:41:41 +1100

    Fix incorrect comment

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,2 +1,2 @@
-export const base_url = 'https://staff.microcomaustralia.com.au';  // URL to web api
+export const base_url = 'https://staff.microcomaustralia.com.au';  // Prefix for images
 export const api_url = 'https://staff.microcomaustralia.com.au/photos/api/';  // URL to web api
