9067f086bb7780b4cfd8ba7680e1f3a32032433e
Brian May <brian@linuxpenguins.xyz>
2020-08-06 11:34:43 +1000

    Update URLS

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,5 +1,5 @@
-export const base_url = 'https://spud.linuxpenguins.xyz';  // Prefix for images
-export const api_url = 'https://spud.linuxpenguins.xyz/api/';  // URL to web api
+export const base_url = 'https://photos.linuxpenguins.xyz';  // Prefix for images
+export const api_url = 'https://photos.linuxpenguins.xyz/api/';  // URL to web api
 
 // export const base_url = '';  // Prefix for images
 // export const api_url = 'http://localhost:8000/api/';  // URL to web api
